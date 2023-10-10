072a7d64739c6e7b0be60e4af83e7f653fcab5e9
TennTennNie <144678351+TennTennNie@users.noreply.github.com>
2023-10-10 16:35:24 +0800

    Add files via upload

---
+++ OrganizationProfile/OrganizationProfile/StudentInformationClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Cache;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizationProfile
+{
+    class StudentInformationClass
+    {
+        public static int SetAge = 0;
+        public static long SetStudentNo, SetContactNo = 0;
+        public static string SetProgram, SetGender, SetBirthDay, SetFullName = " ";
+    }
+}
---
+++ OrganizationProfile/OrganizationProfile/frmRegistration.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrganizationProfile
+{
+    public partial class frmRegistration : Form
+    {
+        private string _FullName;
+        private int _Age;
+        private long _ContactNo, _StudentNo;
+
+        public frmRegistration()
+        {
+            InitializeComponent();
+        }
+
+        private void frmRegistration_Load(object sender, EventArgs e)
+        {
+            string[] ListOfProgram = new string[]{
+            "BS Information Technology",
+            "BS Computer Science",
+            "BS Information Systems",
+            "BS in Accountancy",
+            "BS in Hospitality Management",
+            "BS in Tourism Management"
+            };
+
+            for (int i = 0; i < 6; i++)
+            {
+                cbPrograms.Items.Add(ListOfProgram[i].ToString());
+            }
+        }
+
+        /////return methods 
+        public long StudentNumber(string studNum)
+        {
+
+            if (!(studNum.Equals("")))
+            {
+                _StudentNo = long.Parse(studNum);
+            }
+            else
+            {
+                throw new ArgumentNullException("There's an Error in Student Number!");
+            }
+
+            return _StudentNo;
+        }
+
+        public long ContactNo(string Contact)
+        {
+            if (Regex.IsMatch(Contact, @"^[0-9]{10,11}$"))
+            {
+                _ContactNo = long.Parse(Contact);
+            }
+            else
+            {
+                throw new OverflowException("There's an Error in Contact Number!");
+            }
+            return _ContactNo;
+        }
+
+        public string FullName(string LastName, string FirstName, string MiddleInitial)
+        {
+            if (Regex.IsMatch(LastName, @"^[a-zA-Z]+$") || Regex.IsMatch(FirstName, @"^[a-zA-Z]+$") || Regex.IsMatch(MiddleInitial, @"^[a-zA-Z]+$"))
+            {
+                _FullName = LastName + ", " + FirstName + ", " + MiddleInitial;
+            }
+            else
+            {
+                throw new FormatException("There's an Error in Full Name!");
+            }
+            return _FullName;
+        }
+
+        public int Age(string age)
+        {
+            if (Regex.IsMatch(age, @"^[0-9]{1,3}$"))
+            {
+                _Age = Int32.Parse(age);
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("There's an Error in Age!");
+            }
+            return _Age;
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            try
+            {
+            StudentInformationClass.SetFullName = FullName(txtLastName.Text, txtFirstName.Text, txtMiddleInitial.Text);
+            StudentInformationClass.SetStudentNo = StudentNumber(txtStudentNo.Text);
+            StudentInformationClass.SetProgram = cbPrograms.Text;
+
+            StudentInformationClass.SetGender = cbGender.Text;
+            StudentInformationClass.SetContactNo = ContactNo(txtContactNo.Text);
+            StudentInformationClass.SetAge = Age(txtAge.Text);
+            StudentInformationClass.SetBirthDay = datePickerBirthday.Value.ToString("yyyy- MM - dd");
+
+            frmConfirm frm = new frmConfirm();
+            frm.ShowDialog();
+            }
+            catch(IndexOutOfRangeException ra)
+            {
+                MessageBox.Show(ra.Message);
+            }
+            catch(FormatException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (OverflowException of)
+            {
+                MessageBox.Show(of.Message);
+            }
+            catch (ArgumentNullException Ar)
+            {
+                MessageBox.Show(Ar.Message);
+            }
+        }
+    }
+}
